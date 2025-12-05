f953279cb6503396d380b29be64ed339b92d4329
vignesh-code-git <vignesh.tp44@gmail.com>
2025-12-05 16:09:25 +0530

    next_ecommerce_app_updated

--- app/components/CategoryCard.js
+++ app/components/CategoryCard.js
@@ -1,59 +1,29 @@
 "use client";
 import Link from "next/link";
+import styles from "../module/CategoryCard.module.css";
 
 export const metadata = {
-  title: 'ShopMe',                                      // Tab name
-  description: 'Explore our featured products in My Shop', // Meta description
-  icons: '/google.png',                                 // Favicon (place in /public folder)
+  title: 'ShopMe',
+  description: 'Explore our featured products in My Shop',
+  icons: '/google.png',
 }
 
 export default function CategoryCard({ product }) {
   return (
-    <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-      <div
-        style={{
-          width: "300px",
-          height: "400px",
-          border: "1px solid #ccc",
-          borderRadius: "12px",
-          textAlign: "center",
-          padding: "20px",
-          margin: "10px",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "space-around",
-          fontFamily: "arial",
-          marginLeft: "100px"
-        }}
-      >
-        <div>
+    <div className={styles.cardWrapper}>
+      <div className={styles.card}>
+        <div className={styles.imageBox}>
           <img
-            src={product.images[0]}         // Use first image from DummyJSON's images array
+            src={product.images[0]}
             alt={product.title}
-            style={{ maxHeight: "150px", objectFit: "contain" }}
+            className={styles.productImg}
           />
         </div>
 
-        <div>
-          <h2 style={{ fontSize: "18px", fontWeight: "600", margin: "10px 0" }}>
-            {product.title}
-          </h2>
-          <p style={{ fontSize: "16px", color: "#111", marginBottom: "10px" }}>
-            ${product.price}
-          </p>
-          <Link
-            href={`/products/${product.id}`}
-            style={{
-              textDecoration: "none",
-              color: "#fff",
-              background: "#111",
-              padding: "8px 16px",
-              borderRadius: "8px",
-              fontWeight: "500",
-              display: "inline-block",
-            }}
-          >
+        <div className={styles.details}>
+          <h2 className={styles.title}>{product.title}</h2>
+          <p className={styles.price}>${product.price}</p>
+          <Link href={`/products/${product.id}`} className={styles.viewBtn}>
             View Details
           </Link>
         </div>
